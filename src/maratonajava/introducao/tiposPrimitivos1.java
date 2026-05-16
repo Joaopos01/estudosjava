64a110f5321949f1f7fff010bf6637c09666a5ea
joaopos <joaoibml200306@gmail.com>
2026-05-16 00:53:12 -0300

    exercicio tipos primitivos

--- src/maratonajava/introducao/tiposPrimitivos1.java
+++ src/maratonajava/introducao/tiposPrimitivos1.java
@@ -5,17 +5,21 @@ package maratonajava.introducao;
 public class tiposPrimitivos1 {
     public static void main(String[] args){
         // int, double, float, char, byte, short, long, boolean
-        int age = 10;
-        double salarioDouble = 2000;
-        double salarioFloat = 2500;
+        int age = (int) 10000000L;
+        long numeroGrande = 10000000L;
+        double salarioDouble = 2000.0D;
+        float salarioFloat = 2500.0F;
         byte idadeByte = 10;
         short idadeShot = 10;
         boolean verdadeiro = true;
         boolean falso = false;
         char caractere = '\u0041';
 
+        String nome = "João Pedro";
+
         System.out.println("My age: "+age);
         System.out.println(verdadeiro);
         System.out.println("char "+caractere);
+        System.out.println(nome);
     }
 }
---
+++ src/maratonajava/introducao/tiposPrimitivosExercicios.java
@@ -0,0 +1,15 @@
+package maratonajava.introducao;
+
+public class tiposPrimitivosExercicios {
+    public static void main(String[] args) {
+        String nome = "Joao Pedro";
+        String endereço = "Rua Stella";
+        double salario = 8000;
+        String data = "20/05/2026";
+
+        String relatorio = "Eu "+nome+" morando no endereço "+endereço+" confirmo que recebi o salário de "
+                +salario+" na data "+data;
+
+        System.out.println(relatorio);
+    }
+}

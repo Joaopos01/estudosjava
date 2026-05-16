179a3d83a5ca8ee1c9eb2982a2ff004da2df1a44
joaopos <joaoibml200306@gmail.com>
2026-05-15 23:17:25 -0300

    tipos primitivos

---
+++ src/maratonajava/introducao/tiposPrimitivos1.java
@@ -0,0 +1,21 @@
+package maratonajava.introducao;
+
+
+
+public class tiposPrimitivos1 {
+    public static void main(String[] args){
+        // int, double, float, char, byte, short, long, boolean
+        int age = 10;
+        double salarioDouble = 2000;
+        double salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShot = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = '\u0041';
+
+        System.out.println("My age: "+age);
+        System.out.println(verdadeiro);
+        System.out.println("char "+caractere);
+    }
+}
